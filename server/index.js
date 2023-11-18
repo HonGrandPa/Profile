d0a771ca9a3c01cda39149b9095b3261998c258a
d0w0b <d0w0b@d0w0bs-MacBook-Air.local>
2023-11-18 09:55:45 -0800

    fixed the server

--- server/index.js
+++ server/index.js
@@ -9,8 +9,8 @@ app.listen(3000, () => {
   console.log(`connect to port 3000`);
 });
 
-app.use(express.static(path.join(__dirname, '/front/dist')));
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'front', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 })
